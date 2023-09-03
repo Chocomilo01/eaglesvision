5b3a36e477853cbf0cfd81de1a01c31ecb1ae28b
Chocomilo01 <kenechiodoh@gmail.com>
2023-09-03 16:46:09 -0700

    wth bcryptjs

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const Joi = require("joi");
 const express = require("express");
 const { User } = require("../model/userModel");
@@ -10,62 +10,82 @@ const genAuthToken = require("../utils/genAuthToken");
 const router = express.Router()
 
 router.post("/", async (req, res) => {
-
+  try {
+    // Validate the request body
     const schema = Joi.object({
-         firstName: Joi.string().min(3).max(30).required(), 
-         middleName: Joi.string().min(3).max(30).required(), 
-        lastName: Joi.string().min(3).max(30).required(), 
-          email: Joi.string().min(3).max(200).required().email(),
-          password: Joi.string().min(6).max(200).required(),
-          roles: Joi.string(),
-          phone: Joi.string().required(),
-          sex: Joi.string().required(),
-          bvn: Joi.string().required(),
-          homeAddress: Joi.string().required(),
-          passport: Joi.string(),
-          status: Joi.string(),
+      firstName: Joi.string().min(3).max(30).required(),
+      middleName: Joi.string().min(3).max(30).required(),
+      lastName: Joi.string().min(3).max(30).required(),
+      email: Joi.string().min(3).max(200).required().email(),
+      password: Joi.string().min(6).max(200).required(),
+      roles: Joi.string(),
+      phone: Joi.string().required(),
+      sex: Joi.string().required(),
+      bvn: Joi.string().required(),
+      homeAddress: Joi.string().required(),
+      passport: Joi.string(),
+      status: Joi.string(),
     });
 
-    const {error} = schema.validate(req.body);
+    const { error } = schema.validate(req.body);
 
-    if(error) return res.status(400).send(error.details[0].message);
-    //check if user already exist
+    if (error) {
+      return res.status(400).json({
+        success: false,
+        message: error.details[0].message,
+      });
+    }
+
+    // Check if the user already exists
     let user = await User.findOne({ email: req.body.email }).select('-password');
 
-    if(user) return res.status(400).send("User already exist..");
+    if (user) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists.",
+      });
+    }
 
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    // Create a new user
     user = new User({
-firstName: req.body.firstName,
- middleName: req.body.middleName,
- lastName: req.body.lastName,
- email: req.body.email,
- password: req.body.password,
- roles: req.body.roles,
- phone: req.body.phone,
- sex: req.body.sex,
- bvn: req.body.bvn,
- homeAddress: req.body.homeAddress,
- passport: req.body.passport,
- status: req.body.status,
+      firstName: req.body.firstName,
+      middleName: req.body.middleName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: hashedPassword,
+      roles: req.body.roles,
+      phone: req.body.phone,
+      sex: req.body.sex,
+      bvn: req.body.bvn,
+      homeAddress: req.body.homeAddress,
+      passport: req.body.passport,
+      status: req.body.status,
     });
-    
-    const salt = await bcrypt.genSalt(10)
-    user.password = await bcrypt.hash(user.password, salt)
-    
-    user = await user.save()
 
-    const token = genAuthToken(user)
+    // Save the user to the database
+    await user.save();
 
-    res
-    .status(200)
-    .json({
-    message: "Registered Successfully",
-    data: { user, token },
-    success: true,
+    // Generate JWT token
+    const token = genAuthToken(user);
+
+    // Respond with success
+    res.status(200).json({
+      success: true,
+      message: "Registered successfully",
+      data: { user, token },
+    });
+  } catch (error) {
+    console.error("Error during user registration:", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
 });
-
-})
-
 router.get("/:id", async (req, res) => {
     const userId = req.params.id;
   
