83dc413540bd82492cb036d6cfc6cb006fec5523
Chocomilo01 <kenechiodoh@gmail.com>
2023-10-06 01:43:55 -0700

    with change password

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcryptjs");
 const Joi = require("joi");
 const express = require("express");
 const { User } = require("../model/userModel");
-const { Authenticate } = require('../middlewares/authentication');
+const { authenticate } = require('../middlewares/authentication');
 const genAuthToken = require("../utils/genAuthToken");
 
 
@@ -86,6 +86,8 @@ router.post("/", async (req, res) => {
     });
   }
 });
+
+
 router.get("/:id", async (req, res) => {
     const userId = req.params.id;
   
@@ -178,4 +180,63 @@ router.delete("/:id", async (req, res) => {
     }
   });
   
+// Add a new route for changing passwords
+router.post("/change-password", authenticate, async (req, res) => {
+  try {
+    // Validate the request body
+    const schema = Joi.object({
+      currentPassword: Joi.string().min(6).max(200).required(),
+      newPassword: Joi.string().min(6).max(200).required(),
+    });
+
+    const { error } = schema.validate(req.body);
+
+    if (error) {
+      return res.status(400).json({
+        success: false,
+        message: error.details[0].message,
+      });
+    }
+
+    // Find the user by ID
+    const user = await User.findById(req.user._id);
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    // Verify the current password
+    const isMatch = await bcrypt.compare(req.body.currentPassword, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Current password is incorrect",
+      });
+    }
+
+    // Hash the new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.newPassword, salt);
+
+    // Update the user's password
+    user.password = hashedPassword;
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Password changed successfully",
+    });
+  } catch (error) {
+    console.error("Error changing password:", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+});
+
  module.exports = router;
