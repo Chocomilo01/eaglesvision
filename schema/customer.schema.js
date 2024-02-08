c98116172b53e4e0a353dda1e072efae5f50cb19
Chocomilo01 <kenechiodoh@gmail.com>
2024-02-08 01:54:57 -0600

    add customer error

--- schema/customer.schema.js
+++ schema/customer.schema.js
@@ -5,7 +5,7 @@ const customerSchema = Joi.object({
     sex: Joi.string().valid('male', 'female', 'other'),
     bvn: Joi.string().pattern(/^[0-9]+$/).min(10).max(11).required(),
     homeAddress: Joi.string().min(3).max(300),
-    passport: Joi.string().min(3).max(50),
+    //passport: Joi.string().min(3).max(50),
     maritalStatus: Joi.string().valid('Single', 'Married', 'Divorced', 'Widowed'),
     contactAddress: Joi.string().min(3).max(200),
     nextOfKin: Joi.string().min(3).max(100),
